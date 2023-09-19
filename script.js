a20b601641e3b7c9cc90150fc79e665cb15fb3f4
Frank Young <frankyoung7@yahoo.com>
2023-09-19 11:54:24 -0700

    Note names added

--- script.js
+++ script.js
@@ -10,51 +10,107 @@ const naturalNotesFlats = [ "D", "E", "G", "A", "B"];
 const range = ["G3", "A7"];
 
 
-const piano = document.querySelector("#piano");
-
 const app = {
     setupPiano () {
+        const piano = document.querySelector("#piano");
         const allNaturalNotes = this.getAllNaturalNotes(range);
         const pianoWidth = allNaturalNotes.length * whiteKeyWidth; 
 
         const SVG = this.createMainSVG(pianoWidth, pianoHeight);
-        //Add main SVG to piano div
-        piano.appendChild(SVG);
+        
+        
 
         // Add white keys
         let whiteKeyPositionX = 0;
-        for (let i = 0; i < allNaturalNotes.length; i++) {
+
+        allNaturalNotes.forEach((noteName) => {
+            const whiteKeyTextGroup = utils.createSVGElement("g");
             const whiteKey = this.createKey({ className: "white-key", width: whiteKeyWidth, height: pianoHeight });
-            whiteKey.setAttribute("x", whiteKeyPositionX);
-            whiteKey.setAttribute("data-note-name", allNaturalNotes[i]);
+            const text = utils.createSVGElement('text');
+
+            utils.addTextContent(text, noteName);
+            utils.setAttributes(whiteKeyTextGroup, {"width": whiteKeyWidth});
+            utils.setAttributes(text, {
+                "x": whiteKeyPositionX + whiteKeyWidth / 2,
+                "y": 380,
+                "text-anchor": "middle"
+            });
+            utils.setAttributes(whiteKey, {
+                "x": whiteKeyPositionX,
+                "data-note-name": noteName
+            });
+
+            text.classList.add("white-key-text");
+            whiteKeyTextGroup.appendChild(whiteKey);
+            whiteKeyTextGroup.appendChild(text);
+            SVG.appendChild(whiteKeyTextGroup);
+            
+            // Increment spacing between keys
             whiteKeyPositionX += whiteKeyWidth;
-            SVG.appendChild(whiteKey);
-        }
+        });
+        
         // Add black keys
         let blackKeyPositionX = 60;
         allNaturalNotes.forEach((naturalNote, index, array) => {
+            // If last iteration of keys, do not add black key
+            if ( index === array.length - 1) {
+                return;
+            }
+            
+            const blackKeyTextGroup = utils.createSVGElement("g");
             const blackKey = this.createKey({ className: "black-key", width: whiteKeyWidth / 2, height: pianoHeight / 1.6 });
-            blackKey.setAttribute("x", blackKeyPositionX);
+            const flatNameText = utils.createSVGElement("text");
+            const sharpNameText = utils.createSVGElement("text");
 
+            utils.setAttributes(blackKeyTextGroup, {"width": whiteKeyWidth / 2});
+            
             for (let i = 0; i < naturalNotesSharps.length; i++) {
                 let naturalSharpNoteName = naturalNotesSharps[i];
                 let naturalFlatNoteName = naturalNotesFlats[i];
+                
                 if (naturalSharpNoteName === naturalNote[0]) {
-                    blackKey.setAttribute("data-sharp-name", `${ naturalSharpNoteName }#${ naturalNote[1]}`);
-                    blackKey.setAttribute("data-flat-name", `${ naturalFlatNoteName }b${ naturalNote[1]}`);
+                    
+                    utils.setAttributes(blackKey, {
+                        "x": blackKeyPositionX,
+                        "data-sharp-name": `${ naturalSharpNoteName }#${ naturalNote[1]}`,
+                        "data-flat-name": `${ naturalFlatNoteName }b${ naturalNote[1]}`
+                    });
+
+                    utils.setAttributes(sharpNameText, {
+                        "text-anchor": "middle",
+                        "y": 215,
+                        "x": blackKeyPositionX + (whiteKeyWidth / 4)
+                    });
+                    
+                    utils.setAttributes(flatNameText, {
+                        "text-anchor": "middle",
+                        "y": 235,
+                        "x": blackKeyPositionX + (whiteKeyWidth / 4)
+                    });
+
+                    utils.addTextContent(sharpNameText, `${ naturalSharpNoteName}♯`);
+                    utils.addTextContent(flatNameText, `${ naturalFlatNoteName}♭`);
+
+                    flatNameText.classList.add("black-key-text");
+                    sharpNameText.classList.add("black-key-text");
+
+
                     // Add double spacing between  D# and A#
                     if (naturalSharpNoteName === "D" || naturalSharpNoteName === "A") {
                         blackKeyPositionX += whiteKeyWidth * 2;
                     } else {
                         blackKeyPositionX += whiteKeyWidth;
                     }
-                    // If last iteration of keys, do not add black key
-                    if ( index !== array.length - 1) {
-                        SVG.appendChild(blackKey);
-                    }
+                    
+                    blackKeyTextGroup.appendChild(blackKey);
+                    blackKeyTextGroup.appendChild(flatNameText);
+                    blackKeyTextGroup.appendChild(sharpNameText);
                 }
             }
+            SVG.appendChild(blackKeyTextGroup);
         });
+        //Add main SVG to piano div
+        piano.appendChild(SVG);
     },
     createOctave (octaveNumber) {
         const octave = utils.createSVGElement("g");
@@ -66,8 +122,11 @@ const app = {
     createKey ({className, width, height}) {
         const key = utils.createSVGElement("rect");
         key.classList.add(className);
-        key.setAttribute("width", width);
-        key.setAttribute("height", height);
+
+        utils.setAttributes(key, {
+            "width": width,
+            "height": height
+        })
         return key;
     },
     getAllNaturalNotes ([firstNote, lastNote]) {
@@ -104,12 +163,15 @@ const app = {
     },
     createMainSVG(pianoWidth, pianoHeight) {
         const svg = utils.createSVGElement("svg");
-
-        svg.setAttribute("width", "100%");
-        svg.setAttribute("version", "1.1");
-        svg.setAttribute("xmlns", "http:www.w3.org/2000/svg");
-        svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
-        svg.setAttribute("viewBox", `0 0 ${pianoWidth} ${pianoHeight}`);
+        
+        utils.setAttributes(svg, {
+            "width": "100%",
+            "version": "1.1",
+            "xmlns": "http://www.w3.org/2000/svg",
+            "xmlns:xlink": "http://www.w3.org/1999/xlink",
+            "viewBox": `0 0 ${pianoWidth} ${pianoHeight}`
+        });
+        
 
         return svg;
     }
@@ -119,6 +181,14 @@ const utils = {
     createSVGElement(el) {
         const element = document.createElementNS("http://www.w3.org/2000/svg", el);
         return element;
+    },
+    setAttributes (el, attrs) {
+        for (let key in attrs) {
+            el.setAttribute(key, attrs[key]);
+        }
+    },
+    addTextContent (el, content) {
+        el.textContent = content;
     }
 }
 
