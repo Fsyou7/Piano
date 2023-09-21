5fcad2362da92d20b95d1c10e01978f59d194963
Frank Young <frankyoung7@yahoo.com>
2023-09-21 16:46:13 -0700

    Highlight selected keys

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ const app = {
     },
     createKey ({className, width, height}) {
         const key = utils.createSVGElement("rect");
-        key.classList.add(className);
+        key.classList.add(className, "key");
 
         utils.setAttributes(key, {
             "width": width,
@@ -172,6 +172,22 @@ const app = {
         
 
         return svg;
+    },
+    displayNotes(notes){
+        const pianoKeys = document.querySelectorAll(".key");
+        utils.removeClassFromNodeCollection(pianoKeys, "show");
+
+        notes.forEach(noteName => {
+            pianoKeys.forEach(key => {
+                const naturalName = key.dataset.noteName;
+                const sharpName = key.dataset.sharpName;
+                const flatName = key.dataset.flatName;
+
+                if (naturalName === noteName || sharpName === noteName || flatName === noteName) {
+                    key.classList.add("show");
+                }
+            });
+        });
     }
 }
 
@@ -187,8 +203,16 @@ const utils = {
     },
     addTextContent (el, content) {
         el.textContent = content;
+    },
+    removeClassFromNodeCollection (nodeCollection, classToRemove) {
+        nodeCollection.forEach(node => {
+            if (node.classList.contains(classToRemove)) {
+                node.classList.remove(classToRemove);
+            }
+
+        });
     }
 }
 
 app.setupPiano();
-console.log(app.getAllNaturalNotes(range));
+//app.displayNotes();
